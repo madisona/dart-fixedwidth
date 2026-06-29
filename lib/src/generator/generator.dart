c55e2304db8e91b7468e6da76741cbeca6bbaf19
Aaron Madison <aaron.madison@imtapps.com>
2026-06-29 10:40:40 -0500

    Refactor: Make analyzer more broadly compatible

--- lib/src/generator/generator.dart
+++ lib/src/generator/generator.dart
@@ -21,14 +21,18 @@ class FixedWidthGenerator extends GeneratorForAnnotation<FixedWidthRecord> {
     }
 
     final className = element.name;
+    // ignore: unnecessary_null_comparison
     if (className == null) return '';
     final fieldsInfo = <String>[];
 
     for (final field in element.fields) {
       // Ignore static, synthetic (getters/setters), and private fields
-      if (field.isStatic || field.nonSynthetic != field || field.isPrivate) continue;
+      if (field.isStatic || field.nonSynthetic != field || field.isPrivate) {
+        continue;
+      }
 
       final fieldName = field.name;
+      // ignore: unnecessary_null_comparison
       if (fieldName == null) continue;
 
       final type = field.type;
